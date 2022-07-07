eaa1a5696dca0e18a5bb8adf36edcc03b95b2565
mahrr <ahmadmahrr@protonmail.com>
2022-07-07 09:45:18 +0200

    Add support for compiling let and identifier expressions

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,11 +1,5 @@
 use crate::parser::Expr;
 
-#[derive(Debug)]
-struct GenState {
-    stack_index: u32,
-    environment: Vec<(String, u32)>,
-}
-
 fn generate_label(label: &str) -> String {
     static mut ID: u64 = 0;
     let id: u64;
@@ -16,131 +10,158 @@ fn generate_label(label: &str) -> String {
     format!("{}_{}", label, id)
 }
 
-fn compile_expr(state: &mut GenState, expr: &Expr) -> String {
+fn stack_location(index: u32) -> String {
+    let index = index * 8;
+    format!("[rsp - {index}]")
+}
+
+fn compile_expr(expr: &Expr, stack_index: u32, env: &mut Vec<(String, u32)>) -> String {
+    macro_rules! compile_expr {
+        ($e:expr) => {
+            compile_expr($e, stack_index, env)
+        };
+    }
+
     match expr {
         // literals
         Expr::Boolean(true) => String::from("mov rax, 1"),
         Expr::Boolean(false) => String::from("mov rax, 0"),
         Expr::Integer(number) => format!("mov rax, {}", number),
 
+        // identifier
+        Expr::Identifier(symbol) => {
+            if let Some((_, index)) = env.iter().rev().find(|(name, _)| name == symbol) {
+                format!("mov rax, {}", stack_location(*index))
+            } else {
+                todo!()
+            }
+        }
+
         // arithmetics
         Expr::Add(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
-    add rax, rbx"
+    add rax, {0}",
+                stack_location(stack_index)
             )
         }
         Expr::Sub(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
     mov rbx, rax
-    pop rax
-    sub rax, rbx"
+    mov rax, {0}
+    sub rax, rbx",
+                stack_location(stack_index)
             )
         }
         Expr::Mul(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
-    mul rbx"
+    imul QWORD {0}",
+                stack_location(stack_index)
             )
         }
         Expr::Div(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
     mov rbx, rax
-    pop rax
+    mov rax, {0}
     xor rdx, rdx
-    idiv rbx"
+    idiv rbx",
+                stack_location(stack_index)
             )
         }
 
         // comparison
         Expr::LT(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
+    mov rbx, {0}
     cmp rbx, rax
     mov rax, 0
     mov rbx, 1
-    cmovl rax, rbx"
+    cmovl rax, rbx",
+                stack_location(stack_index)
             )
         }
         Expr::GT(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
+    mov rbx, {0}
     cmp rbx, rax
     mov rax, 0
     mov rbx, 1
-    cmovg rax, rbx"
+    cmovg rax, rbx",
+                stack_location(stack_index)
             )
         }
         Expr::LE(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
+    mov rbx, {0}
     cmp rbx, rax
     mov rax, 0
     mov rbx, 1
-    cmovle rax, rbx"
+    cmovle rax, rbx",
+                stack_location(stack_index)
             )
         }
         Expr::GE(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
+    mov rbx, {0}
     cmp rbx, rax
     mov rax, 0
     mov rbx, 1
-    cmovge rax, rbx"
+    cmovge rax, rbx",
+                stack_location(stack_index)
             )
         }
         Expr::EQ(left, right) => {
-            let left = compile_expr(state, left);
-            let right = compile_expr(state, right);
+            let left = compile_expr(left, stack_index, env);
+            let right = compile_expr(right, stack_index+1, env);
             format!(
                 "    {left}
-    push rax
+    mov {0}, rax
     {right}
-    pop rbx
+    mov rbx, {0}
     cmp rbx, rax
     mov rax, 0
     mov rbx, 1
-    cmove rax, rbx"
+    cmove rax, rbx",
+                stack_location(stack_index)
             )
         }
 
@@ -148,9 +169,9 @@ fn compile_expr(state: &mut GenState, expr: &Expr) -> String {
         Expr::If(cond, then, else_) => {
             let else_label = generate_label("else");
             let end_label = generate_label("if_end");
-            let cond = compile_expr(state, cond);
-            let then = compile_expr(state, then);
-            let else_ = compile_expr(state, else_);
+            let cond = compile_expr!(cond);
+            let then = compile_expr!(then);
+            let else_ = compile_expr!(else_);
             format!(
                 "    {cond}
     cmp rax, 1
@@ -164,17 +185,34 @@ fn compile_expr(state: &mut GenState, expr: &Expr) -> String {
         }
 
         // let
-        Expr::Let(_bindings, _body) => {
-            todo!()
+        Expr::Let(bindings, body) => {
+            let previous_bindings_count = env.len();
+            let mut bindings_ins = String::new();
+            let mut stack_index = stack_index;
+
+            for (name, value) in bindings {
+                let value = compile_expr(value, stack_index, env);
+
+                bindings_ins.push_str(&value);
+                bindings_ins.push_str(&format!("\n    mov {}, rax\n", stack_location(stack_index)));
+
+                env.push((String::clone(name), stack_index));
+                stack_index += 1;
+            }
+
+            let body = compile_expr(body, stack_index, env);
+            env.truncate(previous_bindings_count);
+
+            format!("; bindings
+{bindings_ins}; body
+{body}"
+            )
         }
     }
 }
 
 pub fn compile(expr: &Expr) -> String {
-    let mut state = GenState {
-        stack_index: 0,
-        environment: vec![],
-    };
+    let mut env = vec![];
 
     format!(
         "    section .text
@@ -182,6 +220,6 @@ pub fn compile(expr: &Expr) -> String {
 boot:
 {}
     ret",
-        compile_expr(&mut state, expr)
+        compile_expr(expr, 1, &mut env)
     )
 }
